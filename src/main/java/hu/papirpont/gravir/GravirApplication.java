695db927f7e6ee2fa36432ab04d790d89b7e4c20
Benedek Bence <b3ncib@gmail.com>
2020-01-12 14:39:18 +0100

    :tada: initial commit

---
+++ src/main/java/hu/papirpont/gravir/GravirApplication.java
@@ -0,0 +1,13 @@
+package hu.papirpont.gravir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/papirpont/gravir/GravirApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.papirpont.gravir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
